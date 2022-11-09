29827627a4c14a57e49a1a05dec8cf4417636c52
Sch <junhg0211@gmail.com>
2022-11-09 11:20:57 +0900

    arraylist initial commit

---
+++ list/arraylist/implementation/ArrayList.java
@@ -0,0 +1,78 @@
+package list.arraylist.implementation;
+
+public class ArrayList {
+    private int size = 0;
+    private Object[] elementData = new Object[100];
+
+    public boolean addFirst(Object element) {
+        return add(0, element);
+    }
+
+    public boolean addLast(Object element) {
+        elementData[size] = element;
+        size++;
+
+        return true;
+    }
+
+    public boolean add(int index, Object element) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i-1];
+        }
+        elementData[index] = element;
+        size++;
+
+        return true;
+    }
+
+    public String toString() {
+        String str = "[";
+
+        for (int i = 0; i < size; i++) {
+            str += elementData[i];
+            if (i < size - 1) {
+                str += ", ";
+            }
+        }
+
+        return str + "]";
+    }
+
+    public Object remove(int index) {
+        Object removed = elementData[index];
+
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i+1];
+        }
+        size--;
+        elementData[size] = null;
+
+        return removed;
+    }
+
+    public Object removeFirst() {
+        return remove(0);
+    }
+
+    public Object removeLast() {
+        return remove(size-1);
+    }
+
+    public Object get(int index) {
+        return elementData[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ list/arraylist/implementation/Main.java
@@ -0,0 +1,16 @@
+package list.arraylist.implementation;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList numbers = new ArrayList();
+
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addLast(40);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
+}
